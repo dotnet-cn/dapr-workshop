1485013aabe540de715c681e140e1114fce19848
EdwinVW <edwinvw@gmail.com>
2021-07-04 20:55:00 +0200

    Implement state-management

---
+++ src/TrafficControlService/Repositories/DaprVehicleStateRepository.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using Dapr.Client;
+using TrafficControlService.Models;
+
+namespace TrafficControlService.Repositories
+{
+    public class DaprVehicleStateRepository : IVehicleStateRepository
+    {
+        private const string DAPR_STORE_NAME = "statestore";
+
+        private readonly DaprClient _daprClient;
+
+        public DaprVehicleStateRepository(DaprClient daprClient)
+        {
+            _daprClient = daprClient;
+        }
+
+        public async Task<VehicleState> GetVehicleStateAsync(string licenseNumber)
+        {
+            return await _daprClient.GetStateAsync<VehicleState>(
+                DAPR_STORE_NAME, licenseNumber);
+        }
+
+        public async Task SaveVehicleStateAsync(VehicleState vehicleState)
+        {
+            await _daprClient.SaveStateAsync(
+                DAPR_STORE_NAME, vehicleState.LicenseNumber, vehicleState);
+        }
+    }
+}
